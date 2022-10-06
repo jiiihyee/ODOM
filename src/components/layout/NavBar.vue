2bf17f110190e4237721646c04a0156dbff0c64e
jihye_son <jiiihyeee98@gmail.com>
2022-10-06 10:07:06 +0900

    SNS 컴포넌트화, NavBar 컴포넌트 추가

---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-nav>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Link</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
