ee552f0b7cad9bfb3f96dac129874995b52aa78c
jihye_son <jiiihyeee98@gmail.com>
2022-10-07 16:10:09 +0900

    vwave install

--- src/main.js
+++ src/main.js
@@ -14,14 +14,16 @@ import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import VWave from 'v-wave'
+
+
 
 /* add icons to the library */
 library.add(faUserSecret);
 
 /* add font awesome icon component */
 Vue.component("font-awesome-icon", FontAwesomeIcon);
-
-
+Vue.use(VWave)
 Vue.use(BootstrapVue)
 Vue.use(Vuex)
 Vue.component('default-layout', Default);
