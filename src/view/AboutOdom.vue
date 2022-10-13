19d2c7e6a5c384128abaf659ab286417e113ef2d
jihye_son <jiiihyeee98@gmail.com>
2022-10-13 16:32:36 +0900

    about 설명 ㅋㅋ

--- src/view/AboutOdom.vue
+++ src/view/AboutOdom.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container">
-        about
+        <span> 하루일분은 이건이 만든거임</span>
+        <span> 근데 내가 웹 만들어주고있음</span>
+        <span> 이건은 진짜 복받은 사람인 것 같음...</span>
+        <span> 누가 공짜로 이런거 해주냐?!?</span> 
     </div>
    </template>
 
@@ -11,5 +14,8 @@ export default {
 </script>
 
 <style>
-
+.container{
+    display: flex;
+    flex-direction: column;
+}
 </style>
