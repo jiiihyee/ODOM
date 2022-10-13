fe4e3989608f45f09f207c5f2ba3c66f5953cafa
jihye_son <jiiihyeee98@gmail.com>
2022-10-13 14:17:48 +0900

    카톡 채널 api 가져오기 성공

--- src/view/MainHome.vue
+++ src/view/MainHome.vue
@@ -37,20 +37,34 @@ export default {
       this.kakaoLink();
   },
   methods:{
-    kakaoLink(){
-      window.Kakao.Link.sendDefault({
-        objectType: 'text',
-        text:
-          '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오링크는 다른 템플릿을 이용해 보낼 수 있습니다.',
-        link: {
-          mobileWebUrl:
-            'https://developers.kakao.com',
-          webUrl:
-            'https://developers.kakao.com'
-        }
-      }
-      )
-    }
+    // kakaoLink(){ // 링크 보내기
+    //   window.Kakao.Link.sendDefault({
+    //     objectType: 'text',
+    //     text:
+    //       '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오링크는 다른 템플릿을 이용해 보낼 수 있습니다.',
+    //     link: {
+    //       mobileWebUrl:
+    //         'https://developers.kakao.com',
+    //       webUrl:
+    //         'https://developers.kakao.com'
+    //     }
+    //   }
+    //   )
+    // }
+
+    // kakaoLink(){ //채팅 상담하기 
+    //     window.Kakao.Channel.chat({
+    //         channelPublicId: '_DxipPb' // 카카오톡 채널 홈 URL에 명시된 id로 설정합니다.
+    //         });
+    // }
+
+    // kakaoLink(){ // 채널 추가하기
+    //     window.Kakao.Channel.addChannel({
+    //             channelPublicId: '_DxipPb'
+    //      });
+    // }
+
+
   }
 }
 </script>
