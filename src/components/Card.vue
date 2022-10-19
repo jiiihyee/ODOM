775b0c8690d0dbc118f22612f368eaafada483ef
jihye_son <jiiihyeee98@gmail.com>
2022-10-19 11:17:26 +0900

    제목, 내용 분리 및 표시

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,8 @@
   <div>
     <b-card :img-src=this.item.img img-alt="Card image" img-left class="mb-3">
       <b-card-text>
-        {{item.caption}}
+        <div class="caption">{{this.caption}}</div>
+        <textarea disabled v-model="content" class="content"/>
       </b-card-text>
     </b-card>
 </div>
@@ -17,19 +18,48 @@ export default {
     },
     data(){
         return{
-            
+            caption: this.item.caption.split('\n')[0], //제목
+            content:'',
         }
+    },
+    created(){
+        let con = this.item.caption.split('\n').splice(1).join('')
+        this.content = con
     }
+
 }
 </script>
 
 <style scoped>
 img{
-  width: 500px;
-  height: 500px;
-  object-fit: cover;
+  width: 300px;
+  height: 300px;
+  
 }
-b-card-text{
-    text-overflow:ellipsis;
+.caption{
+    font-weight: bold;
+    font-size: large;
+    margin-bottom: 5%;
+}
+.content{
+  width: 400px;
+  height: 200px;
+  margin: 0 0 0 0;
+}
+.content:disabled{
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 8;
+  resize: none;
+  border: none;
+  background: transparent;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  height: 200px;
+  padding: 3%;
+}
+b-card-body{
+    padding:  0 0 0 0;
+    display: flex;
 }
 </style>
